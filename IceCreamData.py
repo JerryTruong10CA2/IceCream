b9997f427a2e943a7efff18ed6480566ff46f801
JerryTruong10CA2 <125682419+JerryTruong10CA2@users.noreply.github.com>
2023-03-18 21:50:52 +0700

    Update IceCreamData.py

--- IceCreamData.py
+++ IceCreamData.py
@@ -5,6 +5,6 @@ model = pickle.load(open(filename, "rb"))
 st.title('Revenue Prediction')
 a = st.number_input('Input Temperature')
 if st.button('Predict'):
-    y_pred = model.predict(a.reshape(1,-1))
+    y_pred = model.predict(np.array([a]).reshape(1,-1))
     st.caption('Revenue Prediction')
     st.write(y_pred)
