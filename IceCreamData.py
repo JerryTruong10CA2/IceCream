14cde7028b4aacfccfce6f8730e2cd583ed3e7fd
JerryTruong10CA2 <125682419+JerryTruong10CA2@users.noreply.github.com>
2023-03-18 21:44:10 +0700

    Add files via upload

---
+++ IceCreamData.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pickle
+filename = 'IceCreamData.pickle'
+model = pickle.load(open(filename, "rb"))
+st.title('Revenue Prediction')
+a = st.number_input('Input Temperature')
+if st.button('Predict'):
+    y_pred = model.predict(a.reshape(1,-1))
+    st.caption('Revenue Prediction')
+    st.write(y_pred)
